8c8fdcd5696055204fb3179925f374bf27b68291
Bartek <bartosz_janusz731@wp.pl>
2025-01-18 11:29:17 +0100

    dependencies parsing refactoring for better readability

--- main.py
+++ main.py
@@ -13,31 +13,39 @@ def get_pom_dependencies():
     #tree = ET.parse(pom_file)
     tree = ET.parse("example_pom.xml")
     root = tree.getroot()
+    global namespace
     namespace = {'pom': root.tag.split("}")[0].strip("{")}
     dependencies = root.find("pom:dependencies", namespace).findall("pom:dependency", namespace)
     result = {}
     for dependency in dependencies:
-        name = dependency.find("pom:artifactId", namespace).text
-        atributes = {
-            "groupId":
-            dependency.find("pom:groupId", namespace).text if dependency.find("pom:groupId",
-                                                                              namespace) is not None else "",
-            "version":
-            dependency.find("pom:version", namespace).text if dependency.find("pom:version",
-                                                                              namespace) is not None else "",
-            "scope":
-            dependency.find("pom:scope", namespace).text if dependency.find("pom:scope", namespace) is not None else "compile"
-        }
-        result[name] = atributes
+        name, attributes = dependency_parser(dependency)
+        result[name] = attributes
 
     parent = root.find("pom:parent", namespace)
     if parent is not None:
-        for key in result:
-            if result[key]["groupId"] == parent.find("pom:groupId", namespace).text and result[key]["version"] == "":
-                result[key]["version"] = parent.find("pom:version", namespace).text
+        parent = dependency_parser(parent)
+        result = dependency_version_from_parent(result, parent[1])
     return result
 
-
+def dependency_parser(dependency) -> tuple:
+    name = dependency.find("pom:artifactId", namespace).text
+    atributes = {
+        "groupId":
+            dependency.find("pom:groupId", namespace).text if dependency.find("pom:groupId",
+                                                                              namespace) is not None else "",
+        "version":
+            dependency.find("pom:version", namespace).text if dependency.find("pom:version",
+                                                                              namespace) is not None else "",
+        "scope":
+            dependency.find("pom:scope", namespace).text if dependency.find("pom:scope",
+                                                                            namespace) is not None else "compile"
+    }
+    return name, atributes
+def dependency_version_from_parent(dependencies, parent):
+    for key in dependencies:
+        if dependencies[key]["groupId"] == parent["groupId"] and dependencies[key]["version"] == "":
+            dependencies[key]["version"] = parent["version"]
+    return dependencies
 
 
 if __name__ == "__main__":
